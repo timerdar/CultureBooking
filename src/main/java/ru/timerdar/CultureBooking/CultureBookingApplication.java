4b720a8f510841721524ec1b7fce1268bd6c8861
timerdar <timergazin24@mail.ru>
2024-11-16 00:08:25 +0500

    #1 - Init commit

---
+++ src/main/java/ru/timerdar/CultureBooking/CultureBookingApplication.java
@@ -0,0 +1,13 @@
+package ru.timerdar.CultureBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CultureBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CultureBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/timerdar/CultureBooking/CultureBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.timerdar.CultureBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CultureBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
